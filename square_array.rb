c70c6f47ba913e8d2feab87e1516db56c66a7858
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 01:05:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,7 @@
 def square_array(array)
-  #array.each do |element|
-  new_array = element ** 2
- end 
+  new_array = []
+    array.each do |element|
+        new_array << element ** 2
+    end
+    new_array
 end
